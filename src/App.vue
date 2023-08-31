45a1aa148a5afc35a3431dc6163c4c1fbc6e0bb2
Glêsio Santos <glesioss@gmail.com>
2023-08-31 17:24:04 -0300

    adicionando rotas no projeto para navegação

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,12 @@
 <script setup>
-import Profile from './components/Profile.vue'
 
 </script>
 
 <template>
-  <Profile>
-    <h1>GitHub User Data</h1>
-    <template v-slot:footer>
-      <footer>
-        Desenvolvido por <a href="https://github.com/glesiosantos" target="_blank">Glêsio Santos</a>
-      </footer>
-    </template>
-  </Profile>
+  <nav>
+    <RouterLink to="/">Home</RouterLink> 
+    <RouterLink to="/search">Search</RouterLink>
+  </nav>
+  <RouterView></RouterView>
 </template>
 
---
+++ src/views/Search.vue
@@ -0,0 +1,13 @@
+<script setup>
+  import Profile from '../components/Profile.vue'
+</script>
+<template>
+  <Profile>
+    <h1>GitHub User Data</h1>
+    <template v-slot:footer>
+      <footer>
+        Desenvolvido por <a href="https://github.com/glesiosantos" target="_blank">Glêsio Santos</a>
+      </footer>
+    </template>
+  </Profile>
+</template>
