1ede8380c867a3f621a72a110c79293b53a3475b
Glêsio Santos <glesioss@gmail.com>
2023-08-31 17:58:19 -0300

    adicioanando stores na aplicação

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,12 +1,21 @@
 <script setup>
 import { ref } from "vue"
+import { userSearchHistoryStore } from "../stores/userSearchHistoruStore";
+// import {searchHistory} from '../stores/searchHistory'
 
   const emit = defineEmits(['formSubmit', 'update:modelValue'])
   const searchInput = ref('')
 
+  const searchHistory = userSearchHistoryStore()
+
   function handleSubmit(event) {
     event.preventDefault()
+    searchHistory.pushToHistory(searchInput.value)
     emit('formSubmit', searchInput.value)
+  }
+
+  function showSearchHistory(){
+    alert(`Histórico de Pesquisa:\n${searchHistory.users.join('\n')}`)
   }
 
 </script>
@@ -18,5 +27,6 @@ import { ref } from "vue"
       @input="$emit('update:modelValue', $event.target.value)"
     />
     <button>Carregar Usuário</button>
+    <button type="button" @click="showSearchHistory">Carregar Historico</button>
   </form>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,4 +1,26 @@
+<script setup>
+// import { searchHistory } from '../stores/searchHistory'
+
+import { userSearchHistoryStore } from "../stores/userSearchHistoruStore";
+
+const searchHistory = userSearchHistoryStore()
+</script>
 <template>
   <h1>Welcome!</h1>
   <p>This is the homepage of the app.</p>
+
+  <section>
+    <h2>Histório de pesquisa</h2>
+    <p v-for="user in searchHistory.users" :key="user">{{user}}</p>
+  </section>
 </template>
+
+<style scoped>
+section {
+  margin: 1rem 0;
+}
+
+p{
+  margin-top: 1rem;
+}
+</style>
