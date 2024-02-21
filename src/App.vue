ec4d5dcfe91f3503b6ce3cab1f6c5a32b69c9cdb
Glêsio Santos <glesioss@gmail.com>
2024-02-20 21:07:49 -0300

    add tailwind css

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+// import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-
+  <div class="container mx-auto py-5">
+    <h1 className="text-3xl font-bold underline text-white">
+      Hello world!
+    </h1>
+  </div>
 </template>
 
