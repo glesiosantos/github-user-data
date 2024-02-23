a84033a644e24f39575bce61849e9f738a4075f1
Glêsio Santos <glesioss@gmail.com>
2024-02-23 20:57:40 -0300

    add form

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <script setup>
+import { ref } from 'vue';
 import Github from './components/Github.vue'
+
+const isOpen = ref(true)
+
 </script>
 
 <template>
   <div class="container mx-auto py-5 flex flex-col items-center">
-    <Github/>     
+    <input type="checkbox" v-model="isOpen"/><span class="text-white my-2">Is Open</span>
+    <Github v-if="isOpen"/>     
   </div>
 </template>
 
--- src/components/Github.vue
+++ src/components/Github.vue
@@ -1,19 +1,24 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <script setup>
-import { reactive } from "vue"
+import { computed, reactive, ref } from "vue"
+
+const searchInput = ref('')
 
 const state = reactive({
     login: '',
     name: '',
     bio: '...',
-    company: 'Acme Inc.',
-    avatarUrl: 'https://unsplash.it/256',
-    repos: [],
-    searchInput: ''
+    company: '',
+    avatarUrl: '',
+    repos: []
 })
 
+const reposCountMessage = computed(() => state.repos.length > 0 
+    ? `${state.name} possui ${state.repos.length} repositório(s) públicos`
+    : 'Não possui nenhum repositório')
+
 async function fetchGithubUser() {
-    const resp = await fetch(`https://api.github.com/users/${state.searchInput}`)
+    const resp = await fetch(`https://api.github.com/users/${searchInput.value}`)
     const { login, name, bio, company, avatar_url } = await resp.json()
 
     state.login = login
@@ -22,6 +27,7 @@ async function fetchGithubUser() {
     state.company = company
     state.avatarUrl = avatar_url
     fetchUserRepos(login)
+    searchInput.value = ''
 }
 
 async function fetchUserRepos(username) {
@@ -33,26 +39,28 @@ async function fetchUserRepos(username) {
 <template>
     <h1 class="text-red-500 font-bold text-3xl mb-5">Github User Data</h1>
 
-    <div class="flex flex-row w-full px-5 mb-5 space-x-2 justify-center">
-        <input type="text" v-model="state.searchInput" class="text-md w-96 px-2 py-3 rounded-md" />
-        <button @click="fetchGithubUser" class="bg-red-500 px-2 py-3 text-md font-bold text-white rounded-md">Load
-            User</button>
-    </div>
-    <div class="flex flex-col items-center" v-if="state">
+    <p class="text-white ">Pesquisando por: <strong>https://api.github.com/users/{{ searchInput }}</strong></p>
+
+    <form @submit.prevent.enter="fetchGithubUser">
+        <div class="flex flex-row w-full px-5 mb-5 space-x-2 justify-center">
+            <input type="text" v-model.lazy="searchInput" class="text-md w-96 px-2 py-3 rounded-md" />
+            <button type="submit" class="bg-red-500 px-2 py-3 text-md font-bold text-white rounded-md">Load
+                User</button>
+        </div>
+    </form>
+    <div class="flex flex-col items-center px-5" v-if="state.login">
         <img :src="state.avatarUrl" class="w-60 h-60 rounded-full" /> avatar
-        <strong class="text-red-500 ">@{{ state.login }}</strong>
-        <h2 class="text-white">{{ state.name }}</h2>
+        <strong class="text-white ">@{{ state.login }}</strong>
+        <h2 class="text-red-500 text-4xl font-bold my-2">{{ state.name }}</h2>
         <h3 class="text-white">{{ state.company }}</h3>
-        <span class="text-white">{{ state.bio ? state.bio : '...' }}</span>
+        <span class="text-white text-justify my-5">{{ state.bio ? state.bio : '...' }}</span>
     </div>
-    <div>
-        <!-- <p>{{ repos.length }}</p> -->
-        <section v-if="state.repos.length > 0">
-            <article v-for="repo in state.repos" :key="repo" class="flex flex-col items-center">
-                <h3 class="text-white">{{ repo.full_name }}</h3>
-                <p class="text-white">{{ repo.description }}</p>
-                <a :href="repo.html_url" class="text-white" target="_blank">Ver no Github</a>
-            </article>
-        </section>
-    </div>
+    <section v-if="state.repos.length > 0" class="flex flex-col space-y-5 px-5 ">
+        <h2 class="text-white font-thin text-3xl my-2">{{ reposCountMessage }}</h2>
+        <article v-for="repo in state.repos" :key="repo" class="flex flex-col items-center">
+            <h3 class="text-white text-md">{{ repo.full_name }}</h3>
+            <p class="text-white text-justify">{{ repo.description }}</p>
+            <a :href="repo.html_url" class="text-red-500 text-base" target="_blank">Ver no Github</a>
+        </article>
+    </section>
 </template>
